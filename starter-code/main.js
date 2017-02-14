c3d402e1cc42a0d50a98a43bc0dd52e25fa1b133
GoldenDragonMagician <Kenneth.gzhao@outlook.com>
2017-02-14 11:15:58 +0800

    added if else statement to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = 'King';
 var cardTwo = 'queen';
 var cardThree = 'King';
 var cardFour = 'queen';
+
+if (cardOne === cardThree) {
+	alert('You found a match!');
+}
+
+else {
+	alert('Sorry, try again.')
+}
